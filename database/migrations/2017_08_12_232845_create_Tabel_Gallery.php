e157a43e21be9a17b7d38ff9bd767e01f0aae602
Faruq Rahmadani <faruq.rahmadani@yahoo.com>
2017-08-13 07:33:14 +0800

    migration tabel gallery

---
+++ database/migrations/2017_08_12_232845_create_Tabel_Gallery.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTabelGallery extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('Tabel_Gallery', function (Blueprint $table) {
+          $table->increments('id');
+          $table->integer('id_admin');
+          $table->string('judul', 100);
+          $table->string('gambar', 255);
+          $table->string('slide', 255);
+          $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::dropIfExists('Tabel_Gallery');
+    }
+}
