f0ebc6f2472473030f422955842e0776f7a6e015
Faruq Rahmadani <faruq.rahmadani@gmail.com>
2017-08-24 14:55:17 +0800

    update css cetak absensi

--- resources/views/pdf/absensi.blade.php
+++ resources/views/pdf/absensi.blade.php
@@ -1,21 +1,36 @@
+<style>
+table{
+  border-collapse: collapse;
+}
+table, th, td{
+  width: 100%;
+  border:1px solid black;
+}
+td{
+  height: 35px;
+}
+th{
+  text-align: center;
+}
+</style>
 @php
   $no = 0;
 @endphp
-<table style="width:100%; border:1px solid black;">
+<table>
   <thead>
       <tr>
-          <th>No</th>
-          <th>NPM</th>
-          <th>Nama</th>
-          <th>Tanda Tangan</th>
+          <th style="width: 25px;">No</th>
+          <th style="width: 150px;">NPM</th>
+          <th style="width: 300px;">Nama</th>
+          <th style="width: 200px;">Tanda Tangan</th>
       </tr>
   </thead>
   <tbody>
     @foreach ($data as $datas)
       <tr>
-        <td>{{$no = $no+1}}</td>
-        <td>{{$datas->Mahasiswa->NPM}}</td>
-        <td>{{$datas->Mahasiswa->nama}}</td>
+        <td style="text-align: center;">{{$no = $no+1}}</td>
+        <td style="padding-left: 5px;">{{$datas->Mahasiswa->NPM}}</td>
+        <td style="padding-left: 5px;">{{$datas->Mahasiswa->nama}}</td>
         <td></td>
       </tr>
     @endforeach
