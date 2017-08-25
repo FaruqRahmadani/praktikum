0e3eeab7c5185bd0b466aa8aed6c87f2b022a954
Faruq Rahmadani <faruq.rahmadani@gmail.com>
2017-08-25 21:42:34 +0800

    new login

--- resources/views/depan/login.blade.php
+++ resources/views/depan/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" class="body-full-height">
     <head>
         <!-- META SECTION -->
-        <title>Praktikum UNISKA FTI Banjarbaru</title>
+        <title>Program Praktikum FTI UNISKA MAB</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -11,7 +11,7 @@
         <!-- END META SECTION -->
 
         <!-- CSS INCLUDE -->
-        <link rel="stylesheet" type="text/css" id="theme" href="/public-dosen/css/theme-default.css"/>
+        <link rel="stylesheet" type="text/css" id="theme" href="/public-depan/custom/theme-default.css"/>
         <!-- EOF CSS INCLUDE -->
     </head>
     <body>
@@ -21,44 +21,35 @@
             <div class="login-box animated fadeInDown">
                 <div class="login-logo"></div>
                 <div class="login-body">
-                    <div class="login-title"><strong>Welcome</strong>, Please login</div>
+                    <div class="login-title"><strong>Selamat Datang</strong>, Silakan Masuk</div>
                     <form action="{{ route('login') }}" class="form-horizontal" method="post">
-                      <div class="form-group">
-                          <div class="col-md-12">
-                              <input type="text" name="username" class="form-control" placeholder="Username"/ required>
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          <div class="col-md-12">
-                              <input type="password" name="password" class="form-control" placeholder="Password"/ required>
-                          </div>
-                      </div>
+                    <div class="form-group">
+                        <div class="col-md-12">
+                            <input type="text" name="username" class="form-control" placeholder="Username"/ required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-md-12">
+                            <input type="password" name="password" class="form-control" placeholder="Password"/required>
+                        </div>
+                    </div>
                     <div class="form-group">
                         <div class="col-md-6">
-                            {{-- <a href="#" class="btn btn-link btn-block">Forgot your password?</a> --}}
-                            <a href="/"><button class="btn btn-warning btn-block">Kembali</button></a>
-
+                            <a href="#" class="btn btn-link btn-block">Lupa Password?</a>
                         </div>
                         <div class="col-md-6">
                           {{ csrf_field() }}
-                            <button type="submit" class="btn btn-info btn-block">Log In</button>
+                            <button class="btn btn-info btn-block">Masuk</button>
                         </div>
                     </div>
                     </form>
                 </div>
                 <div class="login-footer">
                     <div class="pull-left">
-                        &copy; 2017 JPP
-                    </div>
-                    <div class="pull-right">
-                        <a href="#">About</a> |
-                        <a href="#">Privacy</a> |
-                        <a href="#">Contact Us</a>
+                        &copy; 2017 FTI UNISKA
                     </div>
                 </div>
             </div>
-
         </div>
-
     </body>
 </html>
