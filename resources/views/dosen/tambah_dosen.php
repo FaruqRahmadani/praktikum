b607a1a9f983427821956a9b2a0aca5e6030177f
Faruq Rahmadani <faruq.rahmadani@gmail.com>
2017-08-30 13:28:11 +0800

    fix error "

--- resources/views/dosen/tambah_dosen.php
+++ resources/views/dosen/tambah_dosen.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>        
+    <head>
         <!-- META SECTION -->
-        <title>Praktikum UNISKA FTI Banjarbaru</title>            
+        <title>Praktikum UNISKA FTI Banjarbaru</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href="css/bootstrap.css" rel="stylesheet">
-        
+
         <link rel="icon" href="uniska.ico" type="image/x-icon" />
         <!-- END META SECTION -->
-        
-        <!-- CSS INCLUDE -->        
+
+        <!-- CSS INCLUDE -->
         <link rel="stylesheet" type="text/css" id="theme" href="css/theme-default.css"/>
         <!-- EOF CSS INCLUDE -->
     </head>
     <body>
         <!-- START PAGE CONTAINER -->
         <div class="page-container">
-            
+
             <!-- START PAGE SIDEBAR -->
             <div class="page-sidebar">
                 <!-- START X-NAVIGATION -->
@@ -40,8 +40,8 @@
                                 <a href="detail_dosen.php" data-toggle="tooltip" title="Profil" data-placement="bottom" class="profile-control-left"><span class="glyphicon glyphicon-user"></span></a>
                                 <a href="#" class="profile-control-right"><span class="fa fa-envelope"></span></a>
                             </div>
-                        </div> 
-                    </li>    
+                        </div>
+                    </li>
                     <li class="xn-title">Menu</li>
                     <li>
                         <a href="home.php"><span class="fa fa-home"></span><span class="xn-text">Beranda</span></a>
@@ -73,59 +73,59 @@
                 <!-- END X-NAVIGATION -->
             </div>
             <!-- END PAGE SIDEBAR -->
-            
+
             <!-- PAGE CONTENT -->
             <div class="page-content">
-                
+
                 <!-- START X-NAVIGATION VERTICAL -->
                 <ul class="x-navigation x-navigation-horizontal x-navigation-panel">
                     <!-- TOGGLE NAVIGATION -->
                     <li class="xn-icon-button">
                         <a href="#" data-toggle="tooltip" title="Navigation" data-placement="right" class="x-navigation-minimize"><span class="fa fa-dedent"></span></a>
                     </li>
-                    <!-- END TOGGLE NAVIGATION --> 
+                    <!-- END TOGGLE NAVIGATION -->
                     <!-- SIGN OUT -->
                     <li class="xn-icon-button pull-right">
-                        <a href="#" data-toggle="tooltip" title="Log Out" data-placement="left" class="mb-control" data-box="#mb-signout"><span class="fa fa-sign-out"></span></a>    
-                    </li> 
-                    <!-- END SIGN OUT -->                   
+                        <a href="#" data-toggle="tooltip" title="Log Out" data-placement="left" class="mb-control" data-box="#mb-signout"><span class="fa fa-sign-out"></span></a>
+                    </li>
+                    <!-- END SIGN OUT -->
                 </ul>
-                <!-- END X-NAVIGATION VERTICAL -->                     
-                
+                <!-- END X-NAVIGATION VERTICAL -->
+
                 <!-- START BREADCRUMB -->
                 <ul class="breadcrumb">
                 <li class="active">Menu</li>
-                <li class="active">Proses Data</li>                  
+                <li class="active">Proses Data</li>
                 <li><a href="data_dosen.php" data-toggle="tooltip" title="Go to Data Dosen" data-placement="bottom">Data Dosen</a></li>
                 <li><a href="tambah_dosen.php" data-toggle="tooltip" title="Go to Data Dosen" data-placement="bottom">Tambah Data</a></li>
                 </ul>
-                <!-- END BREADCRUMB -->                
+                <!-- END BREADCRUMB -->
                  <div class="row">
-                 <div class="page-title">                    
+                 <div class="page-title">
                     <h2>Tambah Data</h2>
                 </div>
                 <!-- END PAGE TITLE -->
 
 
             <div class="container">
-                
+
                 <div class="row">
                   <!-- left column -->
                   <div class="col-md-3">
                     <div class="text-center">
-                         <img src="foto/avatar.jpg"" alt="avatar" class="img-rounded img-responsive" class="avatar img-circle" alt="avatar"/>
+                         <img src="foto/avatar.jpg" alt="avatar" class="img-rounded img-responsive" class="avatar img-circle" alt="avatar"/>
                       <h6>Ganti foto</h6>
-                      
+
                       <input type="file" class="form-control">
                     </div>
                   </div>
-                  
+
                   <!-- edit form column -->
                   <div class="col-md-4 personal-info">
                     <div class="alert alert-info alert-dismissable">
-                      <a class="panel-close close" data-dismiss="alert">×</a> 
+                      <a class="panel-close close" data-dismiss="alert">×</a>
                       <h4 style="color: #fff"><i class="fa fa-warning"></i>
-                        Silakan Isi Data Anda.</h4>        
+                        Silakan Isi Data Anda.</h4>
                     </div>
                     <br>
                     <form class="form-horizontal" role="form">
@@ -159,7 +159,7 @@
                           <input class="form-control" type="text" value="">
                         </div>
                       </div>
-                      
+
                       <div class="form-group">
                         <label class="col-md-3 control-label"></label>
                         <div class="col-md-8">
@@ -170,7 +170,7 @@
                     </form>
                   </div>
               </div>
-            </div>   
+            </div>
             <!-- END PAGE CONTENT -->
         </div>
         <!-- END PAGE CONTAINER -->
@@ -180,7 +180,7 @@
                 <div class="mb-middle">
                     <div class="mb-title"><span class="fa fa-times"></span> Hapus Data?</div>
                     <div class="mb-content">
-                        <h4 style="color: #ffffff";>Anda yakin ingin menghapus baris ini?</h4>  
+                        <h4 style="color: #ffffff";>Anda yakin ingin menghapus baris ini?</h4>
                         <h4 style="color: #ffffff";>Tekan Yes jika kamu yakin.</h4>
                     </div>
                     <div class="mb-footer">
@@ -202,7 +202,7 @@
                     <span class="fa fa-sign-out"></span> Keluar ?
                     </div>
                     <div class="mb-content">
-                            <h4 style="color: #ffffff";>Anda yakin ingin keluar?</h4>  
+                            <h4 style="color: #ffffff";>Anda yakin ingin keluar?</h4>
                             <h4 style="color: #ffffff";>Tekan No untuk melanjutkan. Tekan Yes untuk keluar.</h4>
                     </div>
                     <div class="mb-footer">
@@ -219,33 +219,27 @@
        <!-- START PRELOADS -->
         <audio id="audio-alert" src="audio/alert.mp3" preload="auto"></audio>
         <audio id="audio-fail" src="audio/fail.mp3" preload="auto"></audio>
-        <!-- END PRELOADS -->                       
-        
+        <!-- END PRELOADS -->
+
     <!-- START SCRIPTS -->
         <!-- START PLUGINS -->
         <script type="text/javascript" src="js/plugins/jquery/jquery.min.js"></script>
         <script type="text/javascript" src="js/plugins/jquery/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="js/plugins/bootstrap/bootstrap.min.js"></script>        
-        <!-- END PLUGINS -->                
+        <script type="text/javascript" src="js/plugins/bootstrap/bootstrap.min.js"></script>
+        <!-- END PLUGINS -->
 
         <!-- THIS PAGE PLUGINS -->
         <script type='text/javascript' src='js/plugins/icheck/icheck.min.js'></script>
         <script type="text/javascript" src="js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js"></script>
-        <script type="text/javascript" src="js/demo_tables.js"></script> 
-        <script type="text/javascript" src="js/plugins/datatables/jquery.dataTables.min.js"></script>    
+        <script type="text/javascript" src="js/demo_tables.js"></script>
+        <script type="text/javascript" src="js/plugins/datatables/jquery.dataTables.min.js"></script>
         <!-- END PAGE PLUGINS -->
 
         <!-- START TEMPLATE -->
-        <script type="text/javascript" src="js/plugins.js"></script>        
-        <script type="text/javascript" src="js/actions.js"></script>        
+        <script type="text/javascript" src="js/plugins.js"></script>
+        <script type="text/javascript" src="js/actions.js"></script>
         <!-- END TEMPLATE -->
-    <!-- END SCRIPTS --> 
-        
+    <!-- END SCRIPTS -->
+
     </body>
 </html>
-
-
-
-
-
-
